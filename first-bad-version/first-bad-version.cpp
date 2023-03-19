04a3cba8a6be64526173d66658ab76bebde5bb8b
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-03-19 23:03:01 +0530

    Time: 0 ms, Memory: 5.9 MB - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -4,24 +4,24 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-       long long int start = 0 ; 
-        long long int end = n ; 
-        long long int minBadVersion ;
-         
-        while (start <= end)
+        long long start =0;
+        long long end = n;
+        long long int result;
+        while(start <= end)
+            
         {
-            long long int mid = (start + end) / 2 ; 
-
-            if (isBadVersion(mid) == 1)
+    int mid = (start + end)/2;
+            if(isBadVersion(mid) == 1)
             {
-                minBadVersion = mid ; 
-                end = mid - 1 ; 
+                result = mid;
+                end = mid-1;
+                
             }
-            else
-            {
-                start = mid + 1 ; 
+            else{
+                start = mid + 1;
             }
-        }
-        return minBadVersion ;
+            
+            }
+        return result;
     }
 };
