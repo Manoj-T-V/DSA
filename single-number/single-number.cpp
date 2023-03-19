72f9cc722d20560db45d4100da81f51a685ca10f
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-03-19 23:30:18 +0530

    Time: 11 ms, Memory: 16.7 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(auto x : nums)
-        {
-            m[x]++;
-        }
-        
-       for(auto it = m.begin(); it != m.end(); ++it)
-       {
-if(it->second==1)
-{
-return it->first;
-}}
-     return 0;   
+       int x = 0;
+        for(int a: nums)
+            x^=a;
+    
+    return x;
     }
 };
