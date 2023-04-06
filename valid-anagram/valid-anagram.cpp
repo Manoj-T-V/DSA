8ef3b8d53dd9a3f6cfe4fcf84adb4e5e361c925a
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-04-06 10:31:57 +0530

    Time: 4 ms, Memory: 7.4 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
+        int arr[26] = {0};
+        int ss = s.size();
+        int ts = t.size();
+        if(ss!=ts)return false;
+        for(int i = 0;i<ss;i++)
+        {
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+            
+        }
+        
+        for(int i =0;i<26;i++)
+        {
+            if(arr[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
         
     }
 };
