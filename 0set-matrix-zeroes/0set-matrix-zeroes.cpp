e96659aa0df4973f69e5ad60063c0137e722b177
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-04-15 23:01:00 +0530

    Time: 36 ms, Memory: 13.5 MB - LeetHub

---
+++ 0set-matrix-zeroes/0set-matrix-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+            set<int> r;
+            set<int> c;
+            for(int i=0;i<matrix.size();i++){
+                    for(int j=0;j<matrix[0].size();j++){
+                            if(matrix[i][j] == 0)
+                            {
+                                r.insert(i);
+                                    c.insert(j);
+                                    cout << i <<" " << j << endl;
+                            }
+                    }
+            }
+            
+            for(int i=0;i<matrix.size();i++){
+                    for(int j=0;j<matrix[0].size();j++){
+                            if(r.count(i)!=0 || c.count(j)!=0)
+                            {
+                                    cout << "c ";
+                                matrix[i][j]=0;
+                            }
+                    }
+            }
+            
+    }
+};
