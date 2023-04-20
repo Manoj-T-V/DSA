bccf93b7c93c947f302359e5e27540c5eec90920
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-04-20 23:01:41 +0530

    Time: 8 ms, Memory: 13.5 MB - LeetHub

--- search-for-a-range/search-for-a-range.cpp
+++ search-for-a-range/search-for-a-range.cpp
@@ -1,15 +1,51 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-            vector<int>ans;
-           for(int i=0;i<nums.size();i++)
-           {
-                   if(nums[i]==target){
-                           ans.push_back(i);
-                   }
-           }
-            if(ans.empty())return {-1,-1};
-            if(ans.size()==1) return{ans[0],ans[0]};
-        return {ans[0],ans[ans.size()-1]};
+            vector<int>ans = {-1,-1};
+            int start =0;
+            int end = nums.size()-1;
+            int first = -1;
+            int mid = (start+end)/2;
+            //cout<<mid <<" " << end;
+            while(start <= end)
+            {
+                if(target < nums[mid]){
+                        end = mid-1;
+                }
+                    else if(target > nums[mid])
+                    {
+                            start = mid+1;
+                    }
+                    else{
+                           first = mid ;
+                            end = mid-1;
+                    }
+                  mid = (start+end)/2;      
+            }
+            
+            if(first == -1) return ans;
+            else if(first != -1) ans[0] = first;
+            
+            start = 0;
+            end = nums.size()-1;
+            mid = (start+end)/2;
+            int last =first;
+            while(start <= end){
+                if(target < nums[mid]){
+                        end = mid-1;
+                }
+                    else if(target > nums[mid])
+                    {
+                            start = mid+1;
+                    }
+                    else{
+                           last = mid ;
+                            start = mid+1;
+                    }
+                    mid = (start+end)/2;   
+            }
+            ans[1] = last;
+            return ans;
+           
     }
 };
