3ab408ce3d648b2c122d7ab0f1d13945a45cb67d
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-04-21 19:17:52 +0530

    Create rotateMatrixBy90.cpp

---
+++ rotateMatrixBy90.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+        void transpose(vector<vector<int>>& matrix, int r , int c)
+        {
+                for(int i=0;i<r-1;i++)
+                {
+                        for(int j=i;j<c;j++)
+                        {
+                                swap(matrix[i][j],matrix[j][i]);
+                        }
+                }
+        }
+        
+        void rowReverse(vector<vector<int>>& matrix, int r , int c){
+             for(int i=0;i<r;i++)
+                {
+                       reverse(matrix[i].begin(),matrix[i].end());
+                }   
+        }
+        
+    void rotate(vector<vector<int>>& matrix) {
+            int rows = matrix.size();
+            int columns = matrix[0].size();
+            transpose(matrix,rows,columns);
+            rowReverse(matrix,rows,columns);
+    }
+};
