7ca62c2f0e10837d763174f6544717839a3e5445
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-04-21 12:03:21 +0530

    Time: 7 ms, Memory: 6.9 MB - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-    int n = s.length();
-    int start = 0, maxLength = 0;
-    
-    for (int i = 0; i < n; i++) {
-        // Expand around odd-length center
-        int left = i, right = i;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            left--;
-            right++;
-        }
-        int len = right - left - 1;
-        if (len > maxLength) {
-            maxLength = len;
-            start = left + 1;
-        }
+            int n = s.length();
+            int maxlen =0;
+            int start = 0;
+            for(int i=0;i<n;i++){
+                int l = i, r=i;
+                    while(l>=0 && r < n && s[l] == s[r])
+                    {
+                            l--;
+                            r++;
+                    }
+                    int len = r -l -1;
+                    if(maxlen < len)
+                    {
+                            maxlen = len;
+                            start = l+1;
+                    }
+                    
+                    l =i, r = i+1;
+                    while(l>=0 && r<n && s[l]  == s[r])
+                    {
+                        l--;
+                            r++;
+                            
+                    }
+                     len = r -l -1;
+                    if(maxlen < len)
+                    {
+                            maxlen = len;
+                            start = l+1;
+                    }
+                    
+            }
+            
+            return s.substr(start,maxlen);
         
-        // Expand around even-length center
-        left = i, right = i + 1;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            left--;
-            right++;
-        }
-        len = right - left - 1;
-        if (len > maxLength) {
-            maxLength = len;
-            start = left + 1;
-        }
     }
-    
-    return s.substr(start, maxLength);
-}
-
-
 };
