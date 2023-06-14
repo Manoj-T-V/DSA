6109f6d1b0625dc3365d01ff7c2bf30a1097cecd
Manoj T V <71397946+Manoj-T-V@users.noreply.github.com>
2023-06-14 22:40:28 +0530

    Time: 71 ms, Memory: 41.1 MB - LeetHub

--- first-missing-positive/first-missing-positive.cpp
+++ first-missing-positive/first-missing-positive.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-            unordered_map<int,int>um;
-            int max = *max_element(nums.begin(), nums.end());
-            for(auto x : nums)
-            {
-                    um[x]++;
-            }
-            for(int i=1;i<=um.size();i++)
-            {
-                    if(um[i] == 0)
-                    {
-                        return i;
-                    }     
-            }
-        return max+1;
+            std::sort(nums.begin(), nums.end());
+        int missing = 1;
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == missing) missing++;
+        }
+        return missing;
     }
 };
